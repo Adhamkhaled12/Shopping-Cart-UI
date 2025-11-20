68337020e647742ef79f238548f518581b64276f
Adham <adhambackshish12@gmail.com>
2025-11-20 04:11:00 +0200

    Create Cart Context

---
+++ src/context/CartContext.jsx
@@ -0,0 +1,35 @@
+import { createContext, useState, useContext } from "react";
+
+export const CartContext = createContext();
+
+export function CartProvider({ children }) {
+  const [cart, setCart] = useState([]);
+
+  // It takes the previous state(prev), which is simply the previous array of items
+  const addToCart = (product) => {
+    setCart((prev) => {
+      // Checking to see if the item we are adding exists
+      const existing = prev.find((item) => item.id === product.id);
+
+      // If it is True (exists)
+      if (existing) {
+        // We are mapping over the items seeing if the item id matches the product id, if it does we return object with quantity incremented
+        return prev.map((item) =>
+          item.id === product.id ? { ...item, qty: item.qty + 1 } : item
+        );
+      }
+      // Return the previous values plus the new object
+      return [...prev, { ...product, qty: 1 }];
+    });
+  };
+
+  return (
+    <CartContext.Provider value={{ cart, addToCart }}>
+      {children}
+    </CartContext.Provider>
+  );
+}
+
+export function useCart() {
+  return useContext(CartContext);
+}
